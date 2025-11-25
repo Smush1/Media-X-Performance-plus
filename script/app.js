6846c985531100c9fba61ef6b3064f014e14bc0c
sumeshk1 <sumesh1712@gmail.com>
2025-11-25 18:02:09 +0530

    remove borders

--- script/app.js
+++ script/app.js
@@ -180,3 +180,12 @@ const animPara= document.querySelector('.anim__para')
 
 
 
+gsap.set(".cursor", {xPercent: -50, yPercent: -50});
+
+let xTo = gsap.quickTo(".cursor", "x", {duration: 0.6, ease: "power3"}),
+    yTo = gsap.quickTo(".cursor", "y", {duration: 0.6, ease: "power3"});
+
+window.addEventListener("mousemove", e => {
+  xTo(e.clientX);
+  yTo(e.clientY);
+});
